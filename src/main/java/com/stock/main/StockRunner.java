a87c5e2324c46791007cfd89c01ac9cf6324e672
Dmitri Grecov <Dmitri.Grecov@enghouse.com>
2018-03-08 09:19:08 +0100

    first commit

---
+++ src/main/java/com/stock/main/StockRunner.java
@@ -0,0 +1,25 @@
+package com.stock.main;
+
+import java.util.stream.Stream;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StockRunner implements CommandLineRunner{
+	
+	private StockRepository stockRepository;
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		System.out.println("I HAVE STARTED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		Stream.of("Stock1", "Stock2", "Stock3").forEach(stockName -> stockRepository.save(new Stock(stockName)));
+		stockRepository.findAll().stream().forEach(stock -> System.out.println(stock.getName()));
+		
+	}
+
+	public StockRunner(StockRepository stockRepository) {
+		super();
+		this.stockRepository = stockRepository;
+	}
+}
