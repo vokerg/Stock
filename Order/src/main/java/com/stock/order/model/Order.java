510aff73cb9b3d53cc08c8029748e96bf2c06939
Dmitri Grecov <vokerg@gmail.com>
2018-03-17 20:17:35 +0100

    order project

---
+++ Order/src/main/java/com/stock/order/model/Order.java
@@ -0,0 +1,77 @@
+package com.stock.order.model;
+
+import java.util.Date;
+
+public class Order {
+	private int id;
+	private Date date;
+	private int stockId;
+	private int productId;
+	private int stockId2;
+	private float qty;
+	private int operationTypeId;
+	private String stockName;
+	private String stock2Name;
+	private String productName;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public int getStockId() {
+		return stockId;
+	}
+	public void setStockId(int stockId) {
+		this.stockId = stockId;
+	}
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public int getStockId2() {
+		return stockId2;
+	}
+	public void setStockId2(int stockId2) {
+		this.stockId2 = stockId2;
+	}
+	public float getQty() {
+		return qty;
+	}
+	public void setQty(float qty) {
+		this.qty = qty;
+	}
+	public int getOperationTypeId() {
+		return operationTypeId;
+	}
+	public void setOperationTypeId(int operationTypeId) {
+		this.operationTypeId = operationTypeId;
+	}
+	public String getStockName() {
+		return stockName;
+	}
+	public void setStockName(String stockName) {
+		this.stockName = stockName;
+	}
+	public String getStock2Name() {
+		return stock2Name;
+	}
+	public void setStock2Name(String stock2Name) {
+		this.stock2Name = stock2Name;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+}
