f26ac32aeacebf760c93a5df6800857d3bd0b2f8
Dmitri Grecov <vokerg@gmail.com>
2018-04-15 21:30:20 +0200

    adding swagger2 support

--- order/src/main/java/com/stock/order/OrderApplication.java
+++ order/src/main/java/com/stock/order/OrderApplication.java
@@ -5,11 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableSwagger2
 @Import(RabbitConfiguration.class)
 public class OrderApplication {
 
