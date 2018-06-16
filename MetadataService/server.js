193ae75086f395039e8940d564bc26063c78e90f
Dmitri Grecov <vokerg@gmail.com>
2018-06-16 21:38:47 +0200

    file upload tries

--- MetadataService/server.js
+++ MetadataService/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const Eureka = require('eureka-js-client').Eureka;
+const multer  = require('multer');
+const fileSystem = require('fs');
+
 const app = express();
 
 const client = new Eureka({
@@ -24,8 +27,20 @@ const client = new Eureka({
   },
 });
 
+const upload = multer({ dest: 'uploads/' })
+
+app.get('/upload', (req, res) => {
+  res.sendFile(__dirname + '/uploads/2f016c0c80f962f5505c5f67b08a4e23');
+});
+
+app.post('/upload', upload.single('pic'), (req, res, next) => {
+  console.log(req.file);
+  console.log(req.body.productId);
+  res.send(null);
+});
+
 const port = 8085;
 app.listen(port, () => {
   client.start(error => console.log("connected to eureka ", error));
-  console.log("Listening carefully on port", port);
+  console.log("Listening on port", port);
 });
